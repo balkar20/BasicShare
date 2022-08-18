26dd3d74974bddeb23561c678f9b79bd84dc7623
vladislav.balkarov <vladislav.balkarov@admortgage.com>
2022-08-18 15:06:34 +0300

    fix mediateR error

--- WebApplication1/EndpointDefinition.cs
+++ WebApplication1/EndpointDefinition.cs
@@ -4,28 +4,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ModProduct.Handlers;
 using ModProduct.Queries;
+using ModProduct.Repositories;
 using Sevices;
 
 namespace WebApplication1;
 
 public class EndpointDefinition : IEndpointDefinition
 {
-    private readonly IMediator _mediator;
-
-    public EndpointDefinition(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
+    // private readonly IMediator _mediator;
+    //
+    // public EndpointDefinition(IMediator mediator)
+    // {
+    //     _mediator = mediator;
+    // }
     
     public void DefineEndpoints(WebApplication app)
     {
         app.MapGet("/", () => "Hello World2!");
-        app.MapGet("/products", (IMediator _mediator) => _mediator.Send(new GetAllProductsQuery()));
+        app.MapGet("/products", ([FromServices]IMediator _mediator) => _mediator.Send(new GetAllProductsQuery()));
         // app.MapGet("/blogs", ([FromServices]   blogService) => blogService.GetBlogs());
     }
     
     public void DefineServices(IServiceCollection services)
     {
-        services.AddScoped<IBlogService, BlogService>();
+        services.AddScoped<IProductRepository, ProductRepository>();
     }
 }
