fb2752d8aad5e50517b5ecec363ffd0298eebdfb
vladislav.balkarov <vladislav.balkarov@admortgage.com>
2022-08-18 20:06:04 +0300

    refactor and automap

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IRepository<TEntity, TModel> where TEntity : class, new()
+    where TModel : class, new()
+{
+    IEnumerable<TModel> GetAll();
+    Task<TModel> AddAsync(TModel entity);
+}
