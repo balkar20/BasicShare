ce08e850a9ab4e3c5983d1359bccc91fbbf1918d
vladislav.balkarov <vladislav.balkarov@admortgage.com>
2022-09-27 21:01:39 +0300

    fix not shown screen

--- IdentityProvider/Client/Pages/Login.razor.cs
+++ IdentityProvider/Client/Pages/Login.razor.cs
@@ -17,21 +17,21 @@ using IdentityProvider.Shared;
 using static System.Net.WebRequestMethods;
 using Mod.Auth.Models;
 
-namespace IdentityProvider.Client.Pages
-{
-    public partial class Login
-    {
-        private HttpClient Http;
+//namespace IdentityProvider.Client.Pages
+//{
+//    public partial class Login
+//    {
+//        private HttpClient Http;
 
-        public Login(HttpClient http)
-        {
-            Http = http;
-        }
+//        public Login(HttpClient http)
+//        {
+//            Http = http;
+//        }
 
-        private int currentCount = 0;
-        private async Task<LoginModel> LogIn()
-        {
-            return await Http.GetFromJsonAsync<LoginModel>("WeatherForecast");
-        }
-    }
-}
+//        private int currentCount = 0;
+//        private async Task<LoginModel> LogIn()
+//        {
+//            return await Http.GetFromJsonAsync<LoginModel>("WeatherForecast");
+//        }
+//    }
+//}
