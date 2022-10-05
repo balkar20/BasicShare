3f6585aff7479820533a015f3596c00e468325e7
vladislav.balkarov <vladislav.balkarov@admortgage.com>
2022-10-05 19:34:57 +0300

    add files for pricing

---
+++ IdentityProvider/Shared/Pricing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProvider.Shared
+{
+    public record Pricing(string? Rate, string? PriceByCommitment15, string? PriceByCommitment30, string? PriceByCommitment45, string? PriceByCommitment60);
+    //{
+    //    public string? GradeAlias { get; set; }
+    //    public string? Rate { get; set; }
+    //    public string? PriceByCommitment15 { get; set; }
+    //    public string? PriceByCommitment30 { get; set; }
+    //    public string? PriceByCommitment45 { get; set; }
+    //    public string? PriceByCommitment60 { get; set; }
+    //}
+}
---
+++ IdentityProvider/Shared/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProvider.Shared
+{
+    public record Product(string ProductAlias, string BusinessChannelAlias, string GradeAlias, List<Pricing> PricingList);
+}
