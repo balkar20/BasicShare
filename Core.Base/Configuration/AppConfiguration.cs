a5356ca9cac06906830843fb5d7cdacf77cf2891
vladislav.balkarov <vladislav.balkarov@admortgage.com>
2023-01-16 01:21:33 +0300

    kub1

--- Core.Base/Configuration/AppConfiguration.cs
+++ Core.Base/Configuration/AppConfiguration.cs
@@ -8,33 +8,6 @@ public class AppConfiguration : BaseConfiguration
     public AppConfiguration(Func<string, string?> getConfigFunc) : base(getConfigFunc)
     {
     }
-
-    // public AppConfiguration(Func<string, string> getConfigFunc): base(getConfigFunc)
-
-    // public bool UseCache { get => GetConfigFuncString("kjkj") == "Y"; }
-    //
-    // public string LokiUrl { get; set; }
-    //
-    // public string LokiUserName { get; set; }
-    //
-    // public string LokiPassword { get; set; }
-    //
-    // public string RedisUrl { get; set; }
-    //
-    // public bool DockerRunning { get; set; }
-    //
-    // public string DbConnection { get; set; }
-    //
-    // public void LoadEnvironment(Func<string, string>  getConfigFunc)
-    // {
-    //     LokiUserName =  getConfigFunc("LOKI_USER_NAME");
-    //     LokiPassword =  getConfigFunc("LOKI_PASSWORD");
-    //     LokiUrl =  getConfigFunc("LOKI_CONNECTION");
-    //     RedisUrl =  getConfigFunc("REDIS_CONNECTION");
-    //     DockerRunning = getConfigFunc("CONTAINER_RUN") == "Y";
-    //     DbConnection =  getConfigFunc("DB_CONNECTION");
-    //     UseCache = getConfigFunc("USE_CACHE") == "Y";
-    // }
     public bool UseCache => GetConfigFuncBool("USE_CACHE");
 
     public string LokiUrl => GetConfigFuncString("LOKI_CONNECTION");
@@ -49,15 +22,4 @@ public class AppConfiguration : BaseConfiguration
 
     public string DbConnection => GetConfigFuncString("DB_CONNECTION");
 
-    // public void LoadEnvironment(Func<string, string>  getConfigFunc)
-    // {
-    //     LokiUserName =  getConfigFunc("LOKI_USER_NAME");
-    //     LokiPassword =  getConfigFunc("LOKI_PASSWORD");
-    //     LokiUrl =  getConfigFunc("LOKI_CONNECTION");
-    //     RedisUrl =  getConfigFunc("REDIS_CONNECTION");
-    //     DockerRunning = getConfigFunc("CONTAINER_RUN") == "Y";
-    //     DbConnection =  getConfigFunc("DB_CONNECTION");
-    //     UseCache = getConfigFunc("USE_CACHE") == "Y";
-    // }
-    
 }
--- ModProductTest/UnitTest1.cs
+++ ModProductTest/UnitTest1.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace ModProductTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task  Test1()
         {
+            var webFactory = new WebApplicationFactory<Program>();
+            var httpClient = webFactory.CreateDefaultClient();
 
+            var response = await httpClient.GetAsync("");
+            var stringResult = await response.Content.ReadAsStringAsync();
         }
     }
 }
