2419d749e4303c9d6dcb3bd76b39f7fef5e9cafd
vladislav.balkarov <balkar02@gmail.com>
2023-02-02 19:50:49 +0300

    V1-add files for git

---
+++ RedisCache/RedisContext.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+namespace RedisCache;
+
+public class RedisContext
+{
+    private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+    public  ConnectionMultiplexer Connection
+    {
+        get
+        {
+            return lazyConnection.Value;
+        }
+    }
+
+    public RedisContext(string connectionUrl)
+    {
+        RedisContext.lazyConnection = new Lazy<ConnectionMultiplexer>(() => 
+        {
+            return ConnectionMultiplexer.Connect(connectionUrl);
+        });
+    }
+}
