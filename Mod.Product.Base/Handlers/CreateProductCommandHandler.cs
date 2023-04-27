b755d01069b56dae81f56bb07b6d6c1dec81c6e1
balkar20 <balkar20@mail.ru>
2023-04-27 22:12:14 +0300

    V - ProducMod refactoring

--- Mod.Product.Base/Handlers/CreateProductCommandHandler.cs
+++ Mod.Product.Base/Handlers/CreateProductCommandHandler.cs
@@ -1,18 +1,42 @@
+using Core.Transfer;
 using MediatR;
 using Mod.Product.Base.Commands;
 using Mod.Product.Interfaces;
 using Mod.Product.Models;
+using Serilog;
 
 namespace Mod.Product.Base.Handlers;
 
-public class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, ProductModel>
+public class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, BaseResponseResult>
 {
-    private readonly IProductRepository _productRepository;
+    private readonly IProductService _productService;
+    private readonly ILogger _logger;
 
-    public CreateProductCommandHandler(IProductRepository productRepository) => _productRepository = productRepository;
-    
-    public async Task<ProductModel> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public CreateProductCommandHandler(IProductService productService, ILogger logger)
     {
-        return await _productRepository.AddAsync(request.Product);
+        _productService = productService;
+        _logger = logger;
+    }
+
+    public async Task<BaseResponseResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        BaseResponseResult responseResult = new BaseResponseResult() { IsSuccess = false };
+
+        try
+        {
+            var serviceResult = await _productService.CreateAsync(request.Product);
+            if (serviceResult != null)
+            {
+                responseResult.IsSuccess = true;
+                responseResult.Message = $"Product : {serviceResult.Name} was created";
+            }
+            
+        }
+        catch(Exception e)
+        {responseResult.Errors.Add($"Error: {e.Message}");
+            if (e.Message != null) _logger.Error(e.Message);
+        }
+
+        return responseResult;
     }
 }
