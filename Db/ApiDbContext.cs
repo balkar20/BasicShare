086ccbcd05109ee7fba2068799a3d6f1c674065b
balkar20 <balkar20@mail.ru>
2023-06-20 17:22:16 +0300

    V - reconfig docker-compose with ef

--- Db/ApiDbContext.cs
+++ Db/ApiDbContext.cs
@@ -1,5 +1,6 @@
 using Core.Base.DataBase.Entities;
 using Core.Base.Models.Enums;
+using Data.Db.Configuration;
 using Data.Db.Maps;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,29 +11,24 @@ public class ApiDbContext : DbContext
     public ApiDbContext(DbContextOptions<ApiDbContext> options)
         : base(options)
     {
-        
+        Database.EnsureCreated();
     }
+
     public DbSet<ProductEntity> Products { get; set; }
     public DbSet<OrderEntity> Orders { get; set; }
     public DbSet<ShipmentEntity> Shipments { get; set; }
-    
-    
+
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        try
-        {
-            new ProductMap(modelBuilder.Entity<ProductEntity>());
-            new OrderMap(modelBuilder.Entity<OrderEntity>());
-            new ShipmentMap(modelBuilder.Entity<ShipmentEntity>());
-            modelBuilder.Entity<ProductEntity>().HasData(new ProductEntity { Id = 1, Description = "Desc", Name = "Tit" });
-            modelBuilder.Entity<OrderEntity>().HasData(new OrderEntity { Id = 1, Description = "Order-Desc", OrderType = OrderType.Product });
-            modelBuilder.Entity<ShipmentEntity>().HasData(new ShipmentEntity { Id = 1, Description = "Order-Desc", Name = "Order-Tit" });
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            throw;
-        }
-        
+        var userConfig = new ProductConfiguration();
+        // new ProductMap(modelBuilder.Entity<ProductEntity>());
+        // new OrderMap(modelBuilder.Entity<OrderEntity>());
+        // new ShipmentMap(modelBuilder.Entity<ShipmentEntity>());
+        // modelBuilder.Entity<ProductEntity>().HasData(new ProductEntity { Id = 1, Description = "Desc", Name = "Tit" });
+        // modelBuilder.Entity<OrderEntity>().HasData(new OrderEntity { Id = 1, Description = "Order-Desc", OrderType = OrderType.Product });
+        // modelBuilder.Entity<ShipmentEntity>().HasData(new ShipmentEntity { Id = 1, Description = "Order-Desc", Name = "Order-Tit" });
+        modelBuilder.ApplyConfiguration(userConfig);
+        base.OnModelCreating(modelBuilder);
     }
 }
