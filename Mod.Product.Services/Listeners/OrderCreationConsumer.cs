4aedbaf7884abceea9bf8f28e5aa83a374aa4c07
Vladislav <balkar20@mail.ru>
2023-10-12 10:29:52 +0300

    V - dir

--- Mod.Product.Services/Listeners/OrderCreationConsumer.cs
+++ Mod.Product.Services/Listeners/OrderCreationConsumer.cs
@@ -1,12 +1,16 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Mod.Order.EventData.Events;
+using Mod.Product.Interfaces;
+using Mod.Product.Models;
 
 namespace Mod.Product.Services.Listeners;
 
 public class OrderCreationConsumer: IConsumer<OrderCreatedEvent>
 {
     public readonly ILogger<OrderCreationConsumer> _logger;
+    private readonly IServiceScopeFactory scopeFactory;
 
     public OrderCreationConsumer(ILogger<OrderCreationConsumer> logger)
     {
@@ -15,6 +19,15 @@ public class OrderCreationConsumer: IConsumer<OrderCreatedEvent>
 
     public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
     {
+        using var scope = scopeFactory.CreateScope();
+         var repo = scope.ServiceProvider.GetRequiredService<IProductRepository>();
+
+         await repo.AddAsync(new ProductModel()
+         {
+             Name = $"BusinessChannelAlias For {context.Message.Description}",
+             Description = $"ProductAlias For {context.Message.Description}"
+         });
+         
         _logger.LogInformation(context.Message.Description);
     }
 }
